c08030856f4c45df3c166dbb0e39cf76404ba2c2
EndureBlaze <nihaocun@163.com>
2020-11-21 19:02:36 +0800

    Site updated: 2020-11-21 19:02:32

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,13 +1,1 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/hint.css/2.4.1/hint.min.css">var OriginTitle = document.title;
-var titleTime;
-document.addEventListener("visibilitychange", function () {
-    if (document.hidden) {
-        document.title = "╭(°A°`)╮ 页面崩溃啦 ~";
-        clearTimeout(titleTime);
-    } else {
-        document.title = "(｡･ω･｡) 噫又好了~" + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(titleTime)):(document.title="(｡･ω･｡) 噫又好了~"+OriginTitle,titleTime=setTimeout((function(){document.title=OriginTitle}),2e3))}));
